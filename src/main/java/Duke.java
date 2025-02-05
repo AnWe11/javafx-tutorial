162a7ab19a9964e510972567a91385209568b93e
AnWei <e1122931@u.nus.edu>
2025-02-05 16:45:18 +0800

    Reformat styling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Region;
 
 public class Duke extends Application{
 
@@ -19,7 +20,7 @@ public class Duke extends Application{
     private Scene scene;
 
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/Terra.png"));
-    private Image luigiImage = new Image(this.getClass().getResourceAsStream("/images/Terra.png"));
+    private Image luigiImage = new Image(this.getClass().getResourceAsStream("/images/Luigi.png"));
 
     @Override
     public void start(Stage stage) {
@@ -40,13 +41,37 @@ public class Duke extends Application{
         scene = new Scene(mainLayout);
 
         stage.setScene(scene);
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput , 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
+
         stage.show();
 
-        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
-
-        stage.setScene(scene); // Setting the stage to show our scene
-        stage.show(); // Render the stage.
     }
 
 }
